13b9277c10a4bf1cc4ff05ea9d8eec72e0a7bb70
dhruvapanyam <dhruva.panyam_ug21@ashoka.edu.in>
2021-05-14 23:12:38 +0530

    added hyperledger files

---
+++ hyperledger/chaincode/genesistrail/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const GenesisTrail = require('./lib/genesistrail');
+
+module.exports.GenesisTrail = GenesisTrail;
+module.exports.contracts = [ GenesisTrail ];
